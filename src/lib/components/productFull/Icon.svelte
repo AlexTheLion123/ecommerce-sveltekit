8d964931279d8d9e68fe30b88bc338b26d8795d9
Alex <alexurban12@gmail.com>
2022-03-21 23:17:04 +0200

    icons working

---
+++ src/lib/components/productFull/Icon.svelte
@@ -0,0 +1,43 @@
+<script context="module">
+	import { writable } from "svelte/store"; 
+	const currentIcon = writable(undefined);
+</script>
+
+<script>
+	import { onMount } from 'svelte';
+
+	export let imgSrc;
+	export let isCurrent = false;
+
+	let element;
+	function handleClick() {
+		$currentIcon = element;
+	}
+
+	onMount(() => {
+		if (isCurrent) {
+			console.log(element);
+			$currentIcon = element;
+		}
+	});
+</script>
+
+<main bind:this={element} on:click={handleClick} >
+	<img class:selected={$currentIcon === element} src={imgSrc} alt="" />
+</main>
+
+<style>
+
+	img {
+		width: 5rem;
+        border-radius: 5px;
+	}
+
+	img:hover {
+		cursor: pointer;
+	}
+
+	.selected {
+		border: 2px solid rgb(72, 175, 25);
+	}
+</style>
--- src/routes/cannabis/greenhouse/[title].svelte
+++ src/routes/cannabis/greenhouse/[title].svelte
@@ -12,12 +12,53 @@
 </script>
 
 <script>
+	import Icon from '$lib/components/productFull/Icon.svelte';
+
 	export let product;
 </script>
 
-{product}
+<main>
+	<section class="main-img">
+		<img src={product.imgSrc} alt={product.title} />
+	</section>
+	<section class="img-panel">
+		<div class="icon">
+			<Icon imgSrc={product.imgSrc} isCurrent={true} />
+		</div>
 
-This is a product
+		<div class="icon">
+			<Icon imgSrc={product.imgSrc1} />
+		</div>
+
+		<div class="icon">
+			<Icon imgSrc={product.imgSrc2} />
+		</div>
+		<div class="icon">
+			<Icon imgSrc={product.imgSrc3} />
+		</div>
+	</section>
+</main>
 
 <style>
+	main {
+		padding: 2rem;
+		display: grid;
+
+		grid-template: 2fr 1fr;
+		grid-gap: 1rem;
+	}
+
+	.img-panel {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.main-img {
+		width: 30rem;
+	}
+
+	.main-img img {
+		max-width: 100%;
+		max-height: 100%;
+	}
 </style>
